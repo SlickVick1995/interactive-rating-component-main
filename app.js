3e4e5bdebbfc7c77ac90f870c1c17b21ae7ad7eb
SlickVick1995 <36906751+SlickVick1995@users.noreply.github.com>
2024-03-31 11:58:40 -0700

    commit

--- app.js
+++ app.js
@@ -26,6 +26,7 @@
 // }
 
 let ratingBtns = document.querySelectorAll(".rating")
+let rating = null
 
 for (let i = 0; i < ratingBtns.length; i++){
     // button will increment the rating
@@ -35,13 +36,19 @@ for (let i = 0; i < ratingBtns.length; i++){
             _btn.classList.remove('active')
         })
         btn.classList.add('active')
-        
+        rating = i+1
     });
 }
 
 let submitBtn = document.querySelector('#submit')
 
 submitBtn.addEventListener('click', function() {
+    if(rating === null) {
+        alert('Please select a rating')
+        return
+    }
+    let value = document.querySelector('#value');
+    value.innerHTML = `<u>${rating}</u>`;
     let exitScreen = document.querySelector('.exit-screen');
     exitScreen.style.display = 'flex';
     let survey = document.querySelector('.survey');
